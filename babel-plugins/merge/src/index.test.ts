3b1d01e658238489bd092ca6508dd25a72fe2d19
Ben Jervis <bjervis@seek.com.au>
2024-01-27 21:27:32 +1100

    Scaffold a basic plugin

--- babel-plugins/merge/src/index.test.ts
+++ babel-plugins/merge/src/index.test.ts
@@ -1,8 +1,28 @@
-import { describe, expect, it } from "vitest";
-import { testFn } from ".";
+import { expect, it } from "vitest";
+import { transform } from "@babel/core";
+import { merge } from ".";
 
-describe("testFn", () => {
-  it("should return 4", () => {
-    expect(testFn()).toEqual(4);
+const parentCode = `
+  import { secondVar } from "./second.js";
+
+  const firstVar = 'first';
+  const thirdVar = firstVar + secondVar;
+`.trim();
+
+const dependencyCode = `
+  export const secondVar = 'second';
+`.trim();
+
+it("should merge two simple files", () => {
+  const result = transform(parentCode, {
+    plugins: [merge, { path: "./second.js", code: dependencyCode }],
   });
+
+  expect(result).toBeTruthy();
+
+  expect(result?.code).toMatchInlineSnapshot(`
+    const secondVar = 'second';
+    const firstVar = 'first';
+    const thirdVar = firstVar + secondVar;
+  `);
 });
--- babel-plugins/merge/src/index.ts
+++ babel-plugins/merge/src/index.ts
@@ -1,1 +1,13 @@
-export const testFn = () => 4;
+import type * as Babel from "@babel/core";
+
+interface PluginState {
+  opts: { path: string; code: string };
+}
+
+export const merge = (input: typeof Babel): Babel.PluginObj<PluginState> => {
+  return {
+    visitor: {
+      ExportDeclaration(path, state) {},
+    },
+  };
+};
